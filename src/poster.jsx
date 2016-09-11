9207d1fd0631e7a00b0e6c23fbc976dd42cb80fc
Hao Qiao <Hao Qiao>
2016-09-11 16:25:56 -0400

    new waterfall with prevented bounce

--- src/poster.jsx
+++ src/poster.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Article from './article.jsx';
 import $ from 'jquery';
+import uuid from 'uuid';
 
 export default class Poster extends Component {
     constructor() {
         super();
-        this.listNum = 30;
+        var scrollTriggered = false;
+        this.listNum = 18;
         this.articles = null;
         this.state = {
             colume_1: [],
@@ -17,10 +19,11 @@ export default class Poster extends Component {
             this.getList();
         });
         $('#app').scroll(() => {
-            if ($('#app').scrollTop() + $('#app').innerHeight() >= ($('#app')[0].scrollHeight)) {
-                this.listNum += 30;
-                this.getList();
-                console.log('new fetch')
+            if ($('#app').scrollTop() + $('#app').innerHeight() >= ($('#app')[0].scrollHeight) / 1.3 && !scrollTriggered) {
+                scrollTriggered = true;
+                this.listNum += 18;
+                scrollTriggered = this.getList();
+                console.log('%c new fetch', 'color: green');
             }
         });
     }
@@ -31,21 +34,21 @@ export default class Poster extends Component {
                 <div id="colume1">
                     {this.state.colume_1.map((a, i) => {
                         return (
-                            <Article key={a.id} imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
+                            <Article key={uuid.v1()} imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
                         );
                     }) }
                 </div>
                 <div id="colume2">
                     {this.state.colume_2.map((a, i) => {
                         return (
-                            <Article key={a.id} imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
+                            <Article key={uuid.v1()} imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
                         );
                     }) }
                 </div>
                 <div id="colume3">
                     {this.state.colume_3.map((a, i) => {
                         return (
-                            <Article key={a.id} imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
+                            <Article key={uuid.v1()} imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
                         );
                     }) }
                 </div>
@@ -78,11 +81,13 @@ export default class Poster extends Component {
                 }
             }
         }
-        console.log(c1, c2, c3);
+
         this.setState({
             colume_1: c1,
             colume_2: c2,
             colume_3: c3
         });
+
+       return false;
     }
 }
