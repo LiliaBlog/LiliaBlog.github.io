2dff7d2f0d6067a761657d05d2726fa87a74aefe
Hao Qiao <hqiao@hqiao.local>
2016-09-13 12:03:59 -0400

    added reponsive new header

--- src/header.jsx
+++ src/header.jsx
@@ -8,20 +8,7 @@ export default class Header extends Component {
         return (
             <nav id="header" className="navbar navbar-default">
                 <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a className="navbar-brand"><b>SparklingShan's Blog</b></a>
-                    </div>
-                    <div className="collapse navbar-collapse">
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><a>About me</a></li>
-                        </ul>
-                    </div>
+                    <div id="headerTitle">SparklingShan's Blog</div>
                 </div>
             </nav>
         );
