7073a473d93adfc22add078ee192fad3f79ae99e
Hao Qiao <hqiao@hqiao.local>
2016-09-22 11:04:34 -0400

    updated my poster

--- src/myPoster.jsx
+++ src/myPoster.jsx
@@ -11,14 +11,15 @@ export default class Poster extends Component {
         this.listNum = 18;
         this.articles = null;
         this.state = {
-            posts: []
+            colume_1: [],
+            colume_2: []
         };
         $.getJSON("./posts/myPosts.json", (data) => {
             this.articles = data.articles;
             this.getList();
         });
         $(window).scroll(() => {
-            if (disabled === false){
+            if (disabled === false) {
                 if ($(window).scrollTop() + $(window).innerHeight() >= $(document).height() / 1.3 && !scrollTriggered) {
                     scrollTriggered = true;
                     if (this.listNum <= this.articles.length) {
@@ -31,10 +32,10 @@ export default class Poster extends Component {
                 }
             }
         });
-        $(window).on('navOutfits', function(){
+        $(window).on('navOutfits', function () {
             disabled = false;
         });
-        $(window).on('navShop', function(){
+        $(window).on('navShop', function () {
             disabled = true;
         });
     }
@@ -42,19 +43,49 @@ export default class Poster extends Component {
     render() {
         return (
             <div id="myPoster" className="container">
-                {this.state.posts.map((a, i) => {
-                    return (
-                        <MyPost key={uuid.v1() } imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
-                    );
-                }) }
+                <div id="myColume1">
+                    {this.state.colume_1.map((a, i) => {
+                        return (
+                            <MyPost key={uuid.v1() } imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
+                        );
+                    }) }
+                </div>
+                <div id="myColume2">
+                    {this.state.colume_2.map((a, i) => {
+                        return (
+                            <MyPost key={uuid.v1() } imgUrl={a.imgUrl} jumpUrl={a.jumpUrl} title={a.title} description={a.description}/>
+                        );
+                    }) }
+                </div>
             </div>
         );
     }
 
     getList() {
+        var c1 = [];
+        var c2 = [];
+
+        if (this.articles[0]) c1.push(this.articles[0]);
+        if (this.articles[1]) c2.push(this.articles[1]);
+
+        for (var i = 2; i < this.listNum; i++) {
+            if (this.articles[i]) {
+                switch (i % 2) {
+                    case 0:
+                        c1.push(this.articles[i]);
+                        break;
+                    case 1:
+                        c2.push(this.articles[i]);
+                        break;
+                }
+            }
+        }
+
         this.setState({
-            posts: this.articles
+            colume_1: c1,
+            colume_2: c2
         });
+
         return false;
     }
 }
